745c66147d2b44d3b7da0783d9f00c2ece4133c6
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-09 17:41:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-]const express = require('express');
+const express = require('express');
 const path = require('path');
 const { Connection, PublicKey, Keypair } = require('@solana/web3.js');
 const { Pool } = require('pg');
