eab60228b140c44366ea0cbc8e3b0ac0fdc6a9bd
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-10 01:04:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -340,7 +340,3 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
