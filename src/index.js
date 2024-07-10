f33b4097625d5a01ea05d2a2455cbb7a67104223
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-10 19:59:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const pool = new Pool();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(process.cwd(), 'client/build')));
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
