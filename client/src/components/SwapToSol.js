97888c51c2ed9e34f1681b6fadca2488b9d987da
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-10 12:59:42 +0200

    Update SwapToSol.js

--- client/src/components/SwapToSol.js
+++ client/src/components/SwapToSol.js
@@ -1,4 +1,3 @@
-// src/components/SwapToSol.js
 import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
@@ -15,23 +14,6 @@ const Button = styled.button`
 const SwapToSol = () => {
   const handleSwapAll = async () => {
     try {
-      const token = localStorage.getItem('token');
-      await axios.post('/api/swap-all-to-sol', {}, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
+      await axios.post('/api/swap-all-to-sol');
       alert('All assets swapped to SOL successfully');
-    } catch (error) {
-      console.error('Error swapping to SOL:', error);
-      alert('Failed to swap assets to SOL: ' + error.response?.data?.error || error.message);
-    }
-  };
-
-  return (
-    <div>
-      <h3>Swap All Assets to SOL</h3>
-      <Button onClick={handleSwapAll}>Swap All to SOL</Button>
-    </div>
-  );
-};
-
-export default SwapToSol;
+    } catch (error)
